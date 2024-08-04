1828a4a7cec0e147dcd43013340166e62cd524c7
sunwoo <summerham22@gmail.com>
2024-08-05 08:57:22 +0900

    update data columns

---
+++ src/transform/transform.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def transform(load_dt='20200101'):
+    read_df = pd.read_parquet('')
+
+    cols = ['movieCd', #영화의 대표코드를 출력합니다.
+            'movieNm', #영화명(국문)을 출력합니다.
+            'openDt', #영화의 개봉일을 출력합니다.
+            'rank', #해당일자의 박스오피스 순위를 출력합니다.
+            'showCnt', #해당일자에 상영된 횟수를 출력합니다.
+            'audiCnt', #해당일의 관객수를 출력합니다.
+            'salesAmt', #해당일의 매출액을 출력합니다.
+            'audiAcc', #누적관객수를 출력합니다.
+            'salesAcc', #누적매출액을 출력합니다.
+            'salesShare', #해당일자 상영작의 매출총액 대비 해당 영화의 매출비율을 출력합니다.
+            ]
+
+    df = ''
+
