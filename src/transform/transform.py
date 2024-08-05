7cc0400358148468a05b7446d517bce8b135dd73
sunwoo <summerham22@gmail.com>
2024-08-05 14:27:06 +0900

    update transform data function

--- src/transform/transform.py
+++ src/transform/transform.py
@@ -1,8 +1,9 @@
+from extrct.mov import list2df
 import pandas as pd
 
 def transform(load_dt='20200101'):
-    read_df = pd.read_parquet('')
-
+    read_df = list2df()
+    
     cols = ['movieCd', #영화의 대표코드를 출력합니다.
             'movieNm', #영화명(국문)을 출력합니다.
             'openDt', #영화의 개봉일을 출력합니다.
@@ -13,7 +14,19 @@ def transform(load_dt='20200101'):
             'audiAcc', #누적관객수를 출력합니다.
             'salesAcc', #누적매출액을 출력합니다.
             'salesShare', #해당일자 상영작의 매출총액 대비 해당 영화의 매출비율을 출력합니다.
+            'load_dt'
             ]
+    
+    df = read_df[cols]
+    
+    #데이터타입 변환 
+    df['load_dt'] = df['load_dt'].astype('object')
+    df[['rank', 'showCnt', 'audiCnt', 'salesAmt', 'audiAcc', 'salesAcc', 'salesShare']] = df[['rank', 'showCnt', 'audiCnt', 'salesAmt', 'audiAcc', 'salesAcc', 'salesShare']].astype(float).astype(int)
+    df = df.sort_values(by='audiCnt', ascending=False)
+    print(df['audiCnt'])
+    return df
 
-    df = ''
+transform()
+
+
 
